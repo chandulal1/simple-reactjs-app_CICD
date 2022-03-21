08b5922eca2618d0653c39468a1f7fe66ceccb0b
chandulal1 <83697943+chandulal1@users.noreply.github.com>
2022-03-21 14:55:02 +0530

    Update Customers.js

--- src/Customers.js
+++ src/Customers.js
@@ -38,7 +38,7 @@ export default class Customers extends Component {
             </Panel.Heading>
             <Panel.Body>
               <p>{customer.email}</p>
-              <p>{customer.phone number}</p>
+              <p>{customer.phone}</p>
               <Button bsStyle="info" onClick={() => this.setState({selectedCustomer: customer.id})}>
 
                 Click to View Details
